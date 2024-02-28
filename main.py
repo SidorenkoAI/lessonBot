ded1a694e6bcdcb1b0707b97ec0dcbb65599cd4e
sai <sai@mail.ru>
2024-02-28 15:53:22 +0700

    Эхо-бот на чистом API

--- main.py
+++ main.py
@@ -2,19 +2,22 @@ import requests
 import pprint
 with open('token.txt') as f:
     token = f.read()
-#endpoint = 'https://api.telegram.org/bot123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11/getMe'
-# endPoint = f'https://api.telegram.org/bot{token}/getMe'
-# res = requests.get(endPoint).json()
-# pprint.pprint(res)
-endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
-res = requests.get(endPoint).json()
-pprint.pprint(res)
-chatID = []
-for i in res['result']:
-    chatID.append(i['message']['chat']['id'])
 
-print(chatID)
-endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
-for id in chatID:
-    params = {'chat_id': id, 'text': "Привет!!"}
-    res = requests.get(endPoint, params=params).json()
+
+import time
+offset = -2
+while True:
+    #получить информацию по всем событиям (апдейтам)
+    endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
+    param = {'offset': offset + 1}
+    response = requests.get(endPoint, params=param).json()
+    if response['result']:
+        offset = response['result'][0]['update_id']
+        userText = response['result'][0]['message']['text']
+        chatID = response['result'][0]['message']['chat']['id']
+        pprint.pprint(response)
+        endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
+        params = {'chat_id': chatID, 'text': userText}
+        res = requests.get(endPoint, params=params)
+    time.sleep(1)
+
